244f0ddf784c3f67ffda1cfa490fd85a23d40105
whitealcea <44871864+whitealcea@users.noreply.github.com>
2023-06-26 19:26:15 +0900

    dynamic root

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1>404 - Page Not Found</h1>
+      <div>This page is customizable error page</div>
+    </Layout>
+  );
+}
